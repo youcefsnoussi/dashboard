64d35adb6ecdaa607aeb8debcd14441f2384535a
Khaliil <reguieg.ali.k@gmail.com>
2022-09-27 14:27:43 +0100

    detail rc ajouter liste paiments

---
+++ src/main/resources/static/resources/PagesJS/client/list_payment_rc.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+	
+	var h = window.innerHeight;
+	
+	var datatable_config = {
+			"language": {
+		            "url": "resources/Plugins/datatable/lang/French.json"
+		    },
+			"ordering": false,
+	    	"bPaginate": false,
+			"scrollY":"400px",
+			"scrollX": "auto",
+	        "deferRender": true,
+	        initComplete: function () {
+	            this.api().columns([0,2,3,7,8]).every( function () {
+	                var column = this;
+	                var select = $('<select class="form-control" ><option value="">Tout</option></select>')
+	                    .appendTo( $(column.footer()).empty() )
+	                    .on( 'change', function () {
+	                        var val = $.fn.dataTable.util.escapeRegex(
+	                            $(this).val()
+	                        );
+	 
+	                        column
+	                            .search( val ? '^'+val+'$' : '', true, false )
+	                            .draw();
+	                    } );
+	 
+	                column.data().unique().sort().each( function ( d, j ) {
+	                    select.append( '<option value="'+d+'">'+d+'</option>' )
+	                } );
+	            } );
+	        },
+	        drawCallback: function () {
+				
+			      var api = this.api();
+			      
+			      $( api.column( 1, {page:'current'} ).footer() ).html(
+			        api.column( 1, {page:'current'} ).data().sum().formatMoney(2, ',', ' ')
+			      );
+			      
+			     
+			      
+			 },
+			
+    };
+ 		
+ 	var table = $('#table').DataTable(datatable_config);
+//$(".edit_end").click(function() {
+//		
+// 		//console.log("id=>"+$(this).attr("id_relation"))
+// 		var id_rel = $(this).attr("id_relation");
+// 		var end = $(this).parent().find(".end").val();
+// 		
+// 		$.ajax({
+// 			url : 'update_date_end_relation',
+// 			data : {
+// 				id_relation : id_rel,
+// 				end : end
+// 			},
+// 			success : function(responseJson) {
+// 				
+// 				alert("modification effectuée ")
+// 				
+// 			}
+// 		});
+// 		
+//	});
+ 	
+});
+ 	
